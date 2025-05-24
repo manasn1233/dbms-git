d5cba40bac5c181918704976f5086c853fe5a98f
manas <mayurn1233@gmail.com>
2025-05-24 11:11:09 +0530

    Switch to MySQL connection pool to fix closed connection error

--- db.js
+++ db.js
@@ -1,19 +1,16 @@
+require('dotenv').config(); // Load .env variables in local development
+
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT,
+  port: process.env.MYSQL_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error('Database connection failed:', err);
-  } else {
-    console.log('Connected to MySQL database.');
-  }
-});
-
-module.exports = db;
+module.exports = pool;
